60ec026b601292a9068725c0f41d5f362958c540
rexblack <mail@benignware.com>
2014-01-11 00:30:52 +0100

    removed dependency assets

--- config/initializers/simple_form_bootstrap3.rb
+++ config/initializers/simple_form_bootstrap3.rb
@@ -42,50 +42,5 @@ SimpleForm.setup do |config|
     b.use :error, wrap_with: { tag: 'span', class: 'help-block has-error' }
   end
  
-  # config.wrappers :prepend, tag: 'div', class: 'form-group', error_class: 'has-error' do |b|
-    # b.use :html5
-    # b.use :placeholder
-    # b.wrapper tag: 'div', class: 'controls' do |input|
-      # input.wrapper tag: 'div', class: 'input-group' do |prepend|
-        # prepend.use :label , class: 'input-group-addon' ###Please note setting class here fro the label does not currently work (let me know if you know a workaround as this is the final hurdle)
-        # prepend.use :input
-      # end
-      # input.use :hint,  wrap_with: { tag: 'span', class: 'help-block' }
-      # input.use :error, wrap_with: { tag: 'span', class: 'help-block has-error' }
-    # end
-  # end
-#  
-  # config.wrappers :append, tag: 'div', class: 'form-group', error_class: 'has-error' do |b|
-    # b.use :html5
-    # b.use :placeholder
-    # b.wrapper tag: 'div', class: 'controls' do |input|
-      # input.wrapper tag: 'div', class: 'input-group' do |prepend|
-        # prepend.use :input
-    # prepend.use :label , class: 'input-group-addon' ###Please note setting class here fro the label does not currently work (let me know if you know a workaround as this is the final hurdle)
-      # end
-      # input.use :hint,  wrap_with: { tag: 'span', class: 'help-block' }
-      # input.use :error, wrap_with: { tag: 'span', class: 'help-block has-error' }
-    # end
-  # end
-# 
-  # config.wrappers :checkbox, tag: :div, class: "checkbox", error_class: "has-error" do |b|
-# 
-    # # Form extensions
-    # b.use :html5
-# 
-    # # Form components
-    # b.wrapper tag: :label do |ba|
-      # ba.use :input
-      # ba.use :label_text
-    # end
-# 
-    # b.use :hint,  wrap_with: { tag: :p, class: "help-block" }
-    # b.use :error, wrap_with: { tag: :span, class: "help-block text-danger" }
-  # end
- 
-  # Wrappers for forms and inputs using the Twitter Bootstrap toolkit.
-  # Check the Bootstrap docs (http://getbootstrap.com/)
-  # to learn about the different styles for forms and inputs,
-  # buttons and other elements.
   config.default_wrapper = :bootstrap3
 end
--- template.rb
+++ template.rb
@@ -34,9 +34,53 @@ CODE
 append_to_file 'app/assets/javascripts/application.js', "//= require bootstrap\n"
 
 # simple_form bootstrap3 initializer
-copy_file "config/initializers/simple_form_bootstrap3.rb"
+initializer 'simple_form_bootstrap3.rb', <<-CODE
+inputs = %w[
+  CollectionSelectInput
+  DateTimeInput
+  FileInput
+  GroupedCollectionSelectInput
+  NumericInput
+  PasswordInput
+  RangeInput
+  StringInput
+  TextInput
+]
+ 
+inputs.each do |input_type|
+  superclass = "SimpleForm::Inputs::#{input_type}".constantize
+  new_class = Class.new(superclass) do
+    def input_html_classes
+      super.push('form-control')
+    end
+  end
+  Object.const_set(input_type, new_class)
+end
+SimpleForm.setup do |config|
+  
+  config.boolean_style = :nested
+  config.button_class = 'btn btn-default'
+  
+  config.wrappers :bootstrap3, tag: 'div', class: 'form-group', error_class: 'has-error',
+      defaults: { input_html: { class: 'default_class' } } do |b|
+    
+    b.use :html5
+    b.use :min_max
+    b.use :maxlength
+    b.use :placeholder
+    
+    b.optional :pattern
+    b.optional :readonly
+    
+    b.use :label_input
+    b.use :hint,  wrap_with: { tag: 'span', class: 'help-block' }
+    b.use :error, wrap_with: { tag: 'span', class: 'help-block has-error' }
+  end
+ 
+  config.default_wrapper = :bootstrap3
+end
 
-#generate "simple_form:install --bootstrap"
+CODE
 
 # copy layout templates
 run "rm app/views/layouts/application.html.erb"
