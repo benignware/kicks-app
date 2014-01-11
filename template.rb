57b44906f431b11c47fc61689a52fb293736f865
rexblack <mail@benignware.com>
2014-01-11 21:06:20 +0100

    single file template

--- template.rb
+++ template.rb
@@ -43,7 +43,7 @@ inputs = %w[
 ]
  
 inputs.each do |input_type|
-  superclass = "SimpleForm::Inputs::\\#{input_type}".constantize
+  superclass = "SimpleForm::Inputs::\#{input_type}".constantize
   new_class = Class.new(superclass) do
     def input_html_classes
       super.push('form-control')
